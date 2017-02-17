6f9d317b3c58f66abafe93460b2e68a2847ecf65
Fabien Thiombiano <thiombianofabien@gmail.com>
2017-02-16 21:08:44 -0600

    added dfs

--- Graphs/graphs_implementation.js
+++ Graphs/graphs_implementation.js
@@ -8,11 +8,17 @@ function Graph(v){
     this.addEdge = addEdge;
     this.toString = toString;
     this.displayGraph = displayGraph;
+    this.depthFirstSearch = depthFirstSearch;
 
     for (var i=0; i< this.vertices; i++){
         this.adjencyRep[i] = []; //creates the adjency subarray 
         this.adjencyRep[i].push(""); //initialises each array to null
     } 
+    this.marked = []; //array to store visited vertices
+    for(var i=0; i<this.vertices; i++){
+
+        this.marked[i] = false; //initialises all array to false
+    }
 }
 
 //function to add vertices 
@@ -45,15 +51,36 @@ function displayGraph(){
     }
 }
 
+//depthFirstSearch function
+function depthFirstSearch(v){
+
+    this.marked[v] = true;
+
+    if(this.adjencyRep[v] != undefined){
+
+        console.log("Visited vertex: " + v);
+    }
+
+    for(var w in this.adjencyRep[v]){
+
+        if(!this.marked[w]){
+
+            this.depthFirstSearch(w);
+        }
+    }
+}
+
 //test 
 
 var graph = new Graph(5); //5 vertices 
 
-graph.addEdge(0,2); //0,2 edge
-graph.addEdge(2,1); //2,1 edge 
+graph.addEdge(0,1); //0,2 edge
+graph.addEdge(0,2); //2,1 edge 
 graph.addEdge(0,4); //0,4 edge 
-graph.addEdge(4,2); //4,2 edge 
+graph.addEdge(4,2); //4,2 edge
+graph.addEdge(3,1); //3,1 edge
 
 graph.displayGraph();
+graph.depthFirstSearch(0);
 
 
